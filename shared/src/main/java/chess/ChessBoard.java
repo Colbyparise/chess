4f4583902cccaa5cf893946a8eae9a3bcab2d5f7
Colbyparise <cparise@byu.edu>
2025-01-15 22:05:55 -0700

    made classes and small edits

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    private ChessPiece[][] squares = new Chesspiece[8][8];
     public ChessBoard() {
         
     }
@@ -19,10 +19,12 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+
+        squares[position.getRow()][position.getColumn()] = piece;
     }
 
-    /**
+
+     /**
      * Gets a chess piece on the chessboard
      *
      * @param position The position to get the piece from
@@ -30,7 +32,8 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()][position.getColumn()];
+
     }
 
     /**
