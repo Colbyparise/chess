b72019033a7636886240e02f8c573f51a8a1d9bd
Colbyparise <cparise@byu.edu>
2025-01-20 14:22:43 -0700

    Did King and Knight just like Bishop

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 
 public class BishopMovesCalculator {
 
-    @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> validMoves = new ArrayList<>();
 
