35f14077b4e2c0703c88484a664f8a5336294745
Colbyparise <cparise@byu.edu>
2025-03-11 22:03:34 -0600

    SQL connections

--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -1,4 +1,87 @@
 package dataaccess;
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.GameData;
 
-public class SQLGameDAO {
-}
+import java.sql.SQLException;
+import java.util.HashSet;
+
+public class SQLGameDAO implements GameDAO {
+
+    public SQLGameDAO() {
+        try { DatabaseManager.createDatabase(); } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+        try (var conn = DatabaseManager.getConnection()) {
+            var createTestTable = """            
+                    CREATE TABLE if NOT EXISTS game (
+                                    gameID INT NOT NULL,
+                                    whiteUsername VARCHAR(255),
+                                    blackUsername VARCHAR(255),
+                                    gameName VARCHAR(255),
+                                    chessGame TEXT,
+                                    PRIMARY KEY (gameID)
+                                    )""";
+            try (var createTableStatement = conn.prepareStatement(createTestTable)) {
+                createTableStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public HashSet<GameData> listGames() {
+        HashSet<GameData> games = HashSet.newHashSet(16);
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("SELECT gameID, whiteUsername, blackUsername, gameName, chessGame FROM game")) {
+                try (var results = statement.executeQuery()) {
+                    while (results.next()) {
+                        var gameID = results.getInt("gameID");
+                        var whiteUsername = results.getString("whiteUsername");
+                        var blackUsername = results.getString("blackUsername");
+                        var gameName = results.getString("gameName");
+                        var chessGame = deserializeGame(results.getString("chessGame"));
+                        games.add(new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame));
+                    }
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            return null;
+        }
+        return games;
+    }
+
+    @Override
+    public void createGame(GameData game) {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("INSERT INTO game (gameID, whiteUsername, blackUsername, gameName, chessGame) VALUES(?, ?, ?, ?, ?)")) {
+                statement.setInt(1, game.gameID());
+                statement.setString(2, game.whiteUsername());
+                statement.setString(3, game.blackUsername());
+                statement.setString(4, game.gameName());
+                statement.setString(5, serializeGame(game.game()));
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+        }
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("SELECT whiteUsername, blackUsername, gameName, chessGame FROM game WHERE gameID=?")) {
+                statement.setInt(1, gameID);
+                try (var results = statement.executeQuery()) {
+                    results.next();
+                    var whiteUsername = results.getString("whiteUsername");
+                    var blackUsername = results.getString("blackUsername");
+                    var gameName = results.getString("gameName");
+                    var chessGame = deserializeGame(results.getString("chessGame"));
+                    return new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Game not found, id: " + gameID);
+        }
+    }
