6e83cf4fcdae325d4a81b923bd9a5f54c78e119f
Colbyparise <cparise@byu.edu>
2025-03-11 23:25:55 -0600

    BCrypt

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,10 +1,15 @@
 import chess.*;
-
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
 import server.Server;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            DatabaseManager.createDatabase(); } catch(DataAccessException exception){
+            throw new RuntimeException(exception);
+        }
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
         Server server = new Server();
