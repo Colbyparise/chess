dc91fcf68c05e1f4211cd7f0cc1c425eb00194d8
Colbyparise <cparise@byu.edu>
2025-07-20 09:50:50 -0600

    SQLGameDAO and SQLUSerDAO outlines

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,4 +1,66 @@
 package dataaccess;
 
-public class SQLGameDAO {
+
+import model.GameData;
+import chess.ChessGame;
+
+import java.sql.SQLException;
+import java.util.HashSet;
+
+public class SQLGameDAO implements GameDAO {
+    public SQLGameDAO() {
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException exception) {
+            throw new RuntimeException("Error creating database: " + exception.getMessage(), exception);
+        }
+
+        try (var connection = DatabaseManager.getConnection()) {
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS game (" +
+                    "gameID INT NOT NULL, " +
+                    "whiteUsername VARCHAR(255), " +
+                    "blackUsername VARCHAR(255), " +
+                    "gameName VARCHAR(255), " +
+                    "chessGame TEXT, " +
+                    "PRIMARY KEY (gameID)";
+
+            try (var statement = connection.prepareStatement(createTableSQL)) {
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException exception) {
+            throw new RuntimeException("Error setting up game table: " + exception.getMessage(), exception);
+        }
+    }
+
+    @Override
+    public HashSet<GameData> listGames() {
+        HashSet<GameData> games = new HashSet<>(16);
+    }
+
+    @Override
+    public void createGame(GameData gameData) {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame)
+    }
+
+    @Override
+    public boolean gameExists(int gameID) {
+        return true;
+    }
+
+    @Override
+    public void updateGame(GameData gameData) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
 }
+
--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,4 +1,37 @@
 package dataaccess;
 
-public class SQLUserDAO {
+import model.UserData;
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUSerDAO() {
+        initializeDatabase();
+    }
+    private void initializeDatabase() {
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException exception) {
+            throw new RuntimeException("Failed to create database: " + exception.getMessage(), exception);
+        }
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return new UserData(username, password, email);
+    }
+
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+
+    }
+    @Override
+    public boolean authenticateUser(String username, String password) throws DataAccessException {
+        return true;
+    }
+    @Override
+    public void clear() {
+
+    }
+
 }
